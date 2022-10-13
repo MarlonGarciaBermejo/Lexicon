5a6729fe4ce9c9447bfa374a772661b28a81043e
Marlon <yhma22.garcma@folkuniversitetet.nu>
2022-10-13 20:45:13 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Map<String, String> lexicon = new HashMap<>();
+
+        lexicon.put("Allvar", "Serious");
+        lexicon.put("Bestämd", "Determined");
+        lexicon.put("Kortfattad", "Concise");
+        lexicon.put("Särskild", "Particular");
+
+        for (String words : lexicon.keySet()) {
+            System.out.println("Svenska:\n " + words);
+            System.out.println("Engelska:\n" +  lexicon.get(words));
+
+        }
+    }
+}
